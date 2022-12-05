a3dc0f7ffb8c75a3f7372ee86a8a0ba47119c7fd
vsiago <vsiago22@gmail.com>
2022-12-04 22:55:06 -0300

    first commit

---
+++ src/Pages/Site/Site.js
@@ -0,0 +1,11 @@
+import './site.sass'
+
+function Site() {
+  return(
+    <div id="container-site">
+      <h1>Crie seus apps Delvery rápido e simples</h1>
+    </div>
+  )
+}
+
+export default Site
