e0573946b648c29c6847864926f430aab7bb8ce4
vsiago <101620032+vsiago@users.noreply.github.com>
2022-12-07 19:56:16 +0000

    add graphy

--- src/Pages/Site/Site.js
+++ src/Pages/Site/Site.js
@@ -1,5 +1,6 @@
 import './site.sass'
 import Logo from '../../img/logo-criapp.png'
+import Graphy from '../../img/graphy-criapp.png'
 import { Link } from "react-router-dom";
 import ButtonPrimaryLigth from '../../Components/ButtonPrimaryLigth';
 
@@ -11,9 +12,11 @@ function Site() {
           <div id="site">
           <img id="site-logo" src={Logo} alt="Logo Criapp"/>
           <h1>Crie seus apps Delvery simples e rápido</h1>
-          <p>Crie e gerencie aplicativos para sua empresa ou clientes.
-            Compare e aprimore apenas utilizando o celular.</p>
-          <span>Teste grátis!</span>
+          <img id="graphy" src={Graphy} alt="Imagem Criapp"/>
+          <div id="corpo-texto">
+            <p class="corpo-texto">Crie e gerencie aplicativos para sua empresa ou clientes.</p>
+            <p class="corpo-texto" id="paragrafo">Compare e aprimore com o nosso construtor visual de interfaces que a <span id="span-criapp">Criapp</span> oferece.</p>
+          </div>
           <Link to="login"><ButtonPrimaryLigth text="Criar App"></ButtonPrimaryLigth></Link>
       </div>
     </>
